137464edf17337f818974ebfe7448e18f32ad077
Witoslaw Koczewski <wi@koczewski.de>
2018-12-11 10:16:27 +0100

    development environment

---
+++ src/bindscript/demo-spa.cljs
@@ -0,0 +1,21 @@
+(ns bindscript.demo-spa
+  (:require
+   [bindscript.api :refer [def-bindscript]]))
+
+(.log js/console "DEMO SPA")
+
+
+(def-bindscript :some.project/simple
+  test-data   [1 2 3 5 8]
+  incremented (map inc test-data))
+
+
+(def-bindscript :some.project/with-error
+  test-data "some string"
+  result    (conj "some string" "more text"))
+
+
+(def-bindscript :some.project/with-spec
+  k1    :some/keyword
+  :spec qualified-keyword?
+  :spec simple-keyword?)
